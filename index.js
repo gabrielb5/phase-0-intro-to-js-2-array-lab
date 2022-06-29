e35e123d72ec821c12121de5d788dc177d47d300
Gabriel Burgos <gabrielburgosmm@gmail.com>
2022-06-29 10:32:17 -0700

    commit tree 4918e862ad1ddde3c3d279005a79f40c9d4b55d8

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+    return cats }
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+    return cats
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+    return cats
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+    return cats }
+
+function appendCat(name) {
+    let newCats = [...cats]
+    newCats.push(name)
+    return newCats
+}
+
+function prependCat(name) {
+    let newCats = [...cats]
+    newCats.unshift(name)
+    return newCats
+}
+
+function removeLastCat() {
+    let newCats = [...cats]
+    newCats.pop()
+    return newCats
+}
+
+function removeFirstCat() {
+    let newCats = [...cats]
+    newCats.shift()
+    return newCats
+}
